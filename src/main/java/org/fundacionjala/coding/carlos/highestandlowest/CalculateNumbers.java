c8ff049ecfcff2f6d7532e43d3b6ed3919c89ad8
Carlos Meneses <kenshinmc23@gmail.com>
2020-04-26 22:07:30 -0400

    fixing errors about standards

--- src/main/java/org/fundacionjala/coding/carlos/highestandlowest/CalculateNumbers.java
+++ src/main/java/org/fundacionjala/coding/carlos/highestandlowest/CalculateNumbers.java
@@ -6,23 +6,31 @@ import java.util.List;
 
 public class CalculateNumbers {
 
-    public List<Integer> colectNumbers(String numbers) {
+    /**
+     * @param numbers to collect from a given string
+     * @return a list of numbers inside the string numbers
+     */
+    private List<Integer> collectNumbers(final String numbers) {
         List<Integer> numbersList = new ArrayList<>();
         String[] splitNumbers = numbers.split(" ");
-        for (int i=0; i<splitNumbers.length; i++){
+        for (int i = 0; i < splitNumbers.length; i++) {
             numbersList.add(Integer.parseInt(splitNumbers[i]));
         }
         return numbersList;
     }
 
-    public String highestAndLowest(String numbers) {
-       if (numbers.length()<=1){
+    /**
+     * @param numbers to get the highest and lowest
+     * @return two numbers the highest and lowest, the highest always comes first
+     */
+    public String highestAndLowest(final String numbers) {
+       if (numbers.length() <= 1) {
            return numbers;
        }
-       List<Integer> numbersList = colectNumbers(numbers);
+       List<Integer> numbersList = collectNumbers(numbers);
        Collections.sort(numbersList);
-       int highest= numbersList.get(numbersList.size()-1);
-       int lowest= numbersList.get(0);
-       return highest+" "+lowest;
+       int highest = numbersList.get(numbersList.size() - 1);
+       int lowest = numbersList.get(0);
+       return highest + " " + lowest;
     }
 }
--- src/test/java/org/fundacionjala/coding/carlos/highestandlowest/HighestLowestTest.java
+++ src/test/java/org/fundacionjala/coding/carlos/highestandlowest/HighestLowestTest.java
@@ -1,61 +1,41 @@
 package org.fundacionjala.coding.carlos.highestandlowest;
 
 import org.testng.annotations.Test;
-import java.util.ArrayList;
-import java.util.List;
 import static org.testng.Assert.assertEquals;
-import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
 
 public class HighestLowestTest {
 
     @Test
-    public void givenStringOfNumbersThenReturnAListOfNumbers(){
+    public void givenStringOfNumbersWhenCompareThenReceiveTheHighestAndLowest() {
         //given
-        String numbers= "8 -6 7 5";
-        CalculateNumbers calcNumbers = new CalculateNumbers();
-        List<Integer> currentNumbersLists = calcNumbers.colectNumbers(numbers);
-        //when
-        List<Integer> expectedNumbersList = new ArrayList<>();
-        expectedNumbersList.add(8);
-        expectedNumbersList.add(-6);
-        expectedNumbersList.add(7);
-        expectedNumbersList.add(5);
-        //then
-        assertArrayEquals(expectedNumbersList.toArray(), currentNumbersLists.toArray());
-        assertEquals(expectedNumbersList, currentNumbersLists);
-    }
-
-    @Test
-    public void givenStringOfNumbersWhenCompareThenReceiveTheHighestAndLowest(){
-        //given
-        String numbers= "2 -5 3 9 4";
+        String numbers = "2 -5 3 9 4";
         CalculateNumbers calculateNumbers = new CalculateNumbers();
-        String numbersActual= calculateNumbers.highestAndLowest(numbers);
+        String numbersActual = calculateNumbers.highestAndLowest(numbers);
         //when
-        String numbersExpected= "9 -5";
+        String numbersExpected = "9 -5";
         //then
-        assertEquals(numbersActual,numbersExpected);
+        assertEquals(numbersActual, numbersExpected);
     }
     @Test
-    public void givenStringWithNumberWhenCompareThenReceiveTheSameNumber(){
+    public void givenStringWithNumberWhenCompareThenReceiveTheSameNumber() {
         //given
-        String numbers= "7";
+        String numbers = "7";
         CalculateNumbers calculateNumbers = new CalculateNumbers();
-        String numbersActual= calculateNumbers.highestAndLowest(numbers);
+        String numbersActual = calculateNumbers.highestAndLowest(numbers);
         //when
-        String numbersExpected= "7";
+        String numbersExpected = "7";
         //then
-        assertEquals(numbersActual,numbersExpected);
+        assertEquals(numbersActual, numbersExpected);
     }
     @Test
-    public void givenAEmptyStringWhenCompareThenReceiveTheSameNumber(){
+    public void givenAEmptyStringWhenCompareThenReceiveTheSameNumber() {
         //given
-        String numbers= " ";
+        String numbers = " ";
         CalculateNumbers calculateNumbers = new CalculateNumbers();
-        String numbersActual= calculateNumbers.highestAndLowest(numbers);
+        String numbersActual = calculateNumbers.highestAndLowest(numbers);
         //when
-        String numbersExpected= " ";
+        String numbersExpected = " ";
         //then
-        assertEquals(numbersActual,numbersExpected);
+        assertEquals(numbersActual, numbersExpected);
     }
 }
