2d13d02ac13dde14b2d4d504d2c44993cb51efbc
Luqp <luceroqp@outlook.com>
2020-04-29 14:23:30 -0400

    Refactor code, use streams.

--- src/main/java/org/fundacionjala/coding/lucero/Isograms.java
+++ src/main/java/org/fundacionjala/coding/lucero/Isograms.java
@@ -1,8 +1,5 @@
 package org.fundacionjala.coding.lucero;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public final class Isograms {
     private Isograms() {
     }
@@ -12,17 +9,10 @@ public final class Isograms {
             return true;
         }
 
-        String isogramCandidate = word.toLowerCase();
-        Set<Character> helperSet = new HashSet<>();
-
-        for (int i = 0; i < isogramCandidate.length(); i++) {
-            char letter = isogramCandidate.charAt(i);
-
-            if (Character.isDigit(letter) || !helperSet.add(letter)) {
-                return false;
-            }
-        }
-
-        return true;
+        return word.toLowerCase()
+                   .chars()
+                   .distinct()
+                   .filter(Character::isAlphabetic)
+                   .count() == word.length();
     }
 }
