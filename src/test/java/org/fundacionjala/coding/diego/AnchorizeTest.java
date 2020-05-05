9e5e6c66747532f25a2ae6c46c356adf0f3998cf
nanrehd <jdhpp_perezotmail.com>
2020-05-05 15:47:22 -0400

    adding tests

---
+++ src/test/java/org/fundacionjala/coding/diego/AnchorizeTest.java
@@ -0,0 +1,29 @@
+package org.fundacionjala.coding.diego;
+
+import static org.testng.Assert.assertEquals;
+import org.testng.annotations.Test;
+
+public class AnchorizeTest {
+
+    @Test
+    public void givenAFtpUriWhenAnchorizeThenCorrect() {
+        //given
+        String uri = "hello FTP://world.com !";
+        //when
+        String expectedAnchorString = "hello <a href=\"FTP://world.com\">FTP://world.com</a> !";
+        String actualAnchorString = anchor(uri);
+        //then
+        assertEquals(actualAnchorString,expectedAnchorString);
+    }
+
+    @Test
+    public void givenAHttpUriWhenAnchorizeThenCorrect() {
+        //given
+        String uri = "hello http://world.com !";
+        //when
+        String expectedAnchorString = "hello <a href=\"http://world.com\">http://world.com</a> !";
+        String actualAnchorString = anchor(uri);
+        //then
+        assertEquals(actualAnchorString,expectedAnchorString);
+    }
+}
